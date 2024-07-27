c529439563dded8708477a3e91ca5e66f3d88de1
michaelmio333 <israeloahimire@gmail.com>
2024-07-27 03:29:23 +0100

    updated cookie settings

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -9,8 +9,7 @@ const generateTokenAndSetCookie = async (userId, username, res) => {
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 1000,
         httpOnly: true,
-        sameSite: 'None',
-        secure: true
+        secure: process.env.NODE_ENV === "production"
     })
 
     console.log('Cookie set successfully')
