12765162a5c3098c8ea778b2b2afb86984b18cda
michaelmio333 <israeloahimire@gmail.com>
2024-07-27 01:32:46 +0100

    first commit

---
+++ src/utils/generateToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const jwtSecret = process.env.JWT_SECRET
+
+const generateTokenAndSetCookie = async (userId, username, res) => {
+    const token = jwt.sign({userId, username}, jwtSecret, {
+        expiresIn: '15d'
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 1000,
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production'
+    })
+}
+
+module.exports = generateTokenAndSetCookie
